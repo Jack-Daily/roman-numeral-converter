d57c63f449082061acbaa39754ab5893569aee17
Jack-Daily <jd306127@students.davenport.k12.ia.us>
2016-09-29 11:20:01 -0500

    Add files via upload

---
+++ Roman Numeral Converter/Roman Numeral Converter/Form1.Designer.vb
@@ -0,0 +1,121 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtRange = New System.Windows.Forms.TextBox()
+        Me.lblEquivalentNumeral = New System.Windows.Forms.Label()
+        Me.btnConvert = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(46, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(118, 26)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter a decimal integer " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "in the range 1-10:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(46, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(97, 26)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Equivalent Roman " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Numeral:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'txtRange
+        '
+        Me.txtRange.Location = New System.Drawing.Point(181, 38)
+        Me.txtRange.Name = "txtRange"
+        Me.txtRange.Size = New System.Drawing.Size(116, 20)
+        Me.txtRange.TabIndex = 2
+        '
+        'lblEquivalentNumeral
+        '
+        Me.lblEquivalentNumeral.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.lblEquivalentNumeral.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblEquivalentNumeral.Location = New System.Drawing.Point(175, 79)
+        Me.lblEquivalentNumeral.Name = "lblEquivalentNumeral"
+        Me.lblEquivalentNumeral.Size = New System.Drawing.Size(122, 28)
+        Me.lblEquivalentNumeral.TabIndex = 3
+        '
+        'btnConvert
+        '
+        Me.btnConvert.Location = New System.Drawing.Point(164, 141)
+        Me.btnConvert.Name = "btnConvert"
+        Me.btnConvert.Size = New System.Drawing.Size(83, 30)
+        Me.btnConvert.TabIndex = 4
+        Me.btnConvert.Text = "Convert"
+        Me.btnConvert.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(275, 141)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(83, 30)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(49, 141)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(83, 30)
+        Me.btnClear.TabIndex = 6
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(399, 214)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.btnConvert)
+        Me.Controls.Add(Me.lblEquivalentNumeral)
+        Me.Controls.Add(Me.txtRange)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtRange As System.Windows.Forms.TextBox
+    Friend WithEvents lblEquivalentNumeral As System.Windows.Forms.Label
+    Friend WithEvents btnConvert As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+
+End Class
---
+++ Roman Numeral Converter/Roman Numeral Converter/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        txtRange.Clear()
+        lblEquivalentNumeral.Text = String.Empty
+
+
+    End Sub
+
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        Dim decUserInput As Integer
+        '  Dim intNum As Integer
+
+        Try
+            'Convert number to roman numeral
+            decUserInput = CDec(txtRange.Text)
+
+            'Determine Roman numeral
+            Select Case decUserInput
+                Case Is = 1
+                    lblEquivalentNumeral.Text = "I"
+
+                Case Is = 2
+                    lblEquivalentNumeral.Text = "II"
+
+                Case Is = 3
+                    lblEquivalentNumeral.Text = "III"
+
+                Case Is = 4
+                    lblEquivalentNumeral.Text = "IV"
+
+                Case Is = 5
+                    lblEquivalentNumeral.Text = "V"
+
+                Case Is = 6
+                    lblEquivalentNumeral.Text = "VI"
+
+                Case Is = 7
+                    lblEquivalentNumeral.Text = "VII"
+
+                Case Is = 8
+                    lblEquivalentNumeral.Text = "VIII"
+
+                Case Is = 9
+                    lblEquivalentNumeral.Text = "IX"
+
+                Case Is = 10
+                    lblEquivalentNumeral.Text = "X"
+
+
+
+            End Select
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
